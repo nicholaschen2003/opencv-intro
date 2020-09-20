13c8dc0bbc0b46b86366778bdb0d514579db0077
Nicholas Chen <nicholas.chen.21@bishops.com>
2020-09-19 20:52:16 -0700

    added contouring and found heart outline using it

--- project.py
+++ project.py
@@ -23,6 +23,7 @@ cv2.waitKey(0)
 
 #chapter 4: getting and editing pixels, and chapter 5: using opencv methods to edit pic
 smileyFace = image.copy()
+
 #eyes
 smileyFace[int(height/10):int(height/10+10), int(3*width/4):int(3*width/4+10)] = (255, 0, 0)
 smileyFace[int(height/10):int(height/10+10), int(3*width/4+30):int(3*width/4+40)] = (255, 0, 0)
@@ -51,4 +52,19 @@ cv2.waitKey(0)
 edges = cv2.Canny(edges, 30, 150)
 
 cv2.imshow("Edges", edges)
+cv2.waitKey(0)
+
+#chapter 11: contours (builds on previous code)
+(contours, _) = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+contour = image.copy()
+#used to display each individual contour and find heart
+# for cntr in contours:
+#     cv2.drawContours(contour, cntr, -1, (255, 255, 255), 2)
+#     cv2.imshow("Contour", contour)
+#     cv2.waitKey(0)
+
+#heart outline
+cv2.drawContours(contour, contours[15], -1, (255, 255, 0), 7)
+cv2.imshow("Contour", contour)
 cv2.waitKey(0)
